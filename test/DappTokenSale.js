ff6ab932d4251612b630dd9b5c9e19eff2ad60d9
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-02-22 22:29:09 -0400

    Update test/DappTokenSale.js to add tests to check the functionality of the buyTokens method in the DappTokenSale.sol contract. Update the DappTokenSale.sol contract to implement the logic to pass the tests

--- test/DappTokenSale.js
+++ test/DappTokenSale.js
@@ -1,8 +1,16 @@
+var DappToken = artifacts.require("./DappToken.sol");
 var DappTokenSale = artifacts.require("./DappTokenSale.sol");
 
+
 contract("DappTokenSale", function(accounts) {
+  var tokenInstance;
   var tokenSaleInstance;
+  var admin = accounts[0];
+  var buyer = accounts[1];
   var tokenPrice = 1000000000000000; // in wei
+  var tokensAvailable = 750000;
+  var numberOfTokens;
+
 
   it("initializes the contract with the correct values", function() {
 
@@ -20,6 +28,53 @@ contract("DappTokenSale", function(accounts) {
       return tokenSaleInstance.tokenPrice();
     }).then(function(price) {
       assert.equal(price, tokenPrice, "token price is correct");
+    });
+  });
+
+  it("facilitates token buying", function() {
+
+    return DappToken.deployed().then(function(instance) {
+      tokenInstance = instance
+
+      return DappTokenSale.deployed();
+    }).then(function(instance) {
+      tokenSaleInstance = instance;
+
+      return tokenInstance.transfer(tokenSaleInstance.address, tokensAvailable, {from: admin});
+    }).then(function(receipt) {
+      numberOfTokens = 10;
+
+      return tokenSaleInstance.buyTokens(numberOfTokens, { from: buyer, value: numberOfTokens * tokenPrice})
+    }).then(function(receipt){
+
+      assert.equal(receipt.logs.length, 1, "triggers one event");
+
+      assert.equal(receipt.logs[0].event, "Sell", "should be the `Sell` event");
+
+      assert.equal(receipt.logs[0].args._buyer, buyer, "logs the account that purchased the tokens");
+
+      assert.equal(receipt.logs[0].args._amount, numberOfTokens, "logs the number of tokens purchased");
+
+      return tokenSaleInstance.tokensSold();
+    }).then(function(amount) {
+      assert.equal(amount.toNumber(), numberOfTokens, "increments the number of tokens sold");
+
+      return tokenInstance.balanceOf(buyer);
+    }).then(function(balance) {
+      assert.equal(balance.toNumber(),  numberOfTokens);
+
+      return tokenInstance.balanceOf(tokenSaleInstance.address);
+    }).then(function(balance) {
+      assert.equal(balance.toNumber(), tokensAvailable - numberOfTokens);
+
+      return tokenSaleInstance.buyTokens(numberOfTokens, { from: buyer, value: 1})
+    }).then(assert.fail).catch(function(err){
+      assert(err.message.indexOf('revert') >= 0, "msg.value must equal number of tokens in wei");
+
+      return tokenSaleInstance.buyTokens(800000, { from: buyer, value: 1})
+    }).then(assert.fail).catch(function(err){
+      assert(err.message.indexOf('revert') >= 0, "cannot purchase more tokens than available");
     })
-  }); 
+  });
+
 }); 

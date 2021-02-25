901d5a84e82a1c1c936d0f7a37e24db8a630114d
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-02-24 22:31:29 -0400

    Update app.js to have the frontend app connected to the blockchain

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,8 @@
 App = {
+  web3Provider: null,
+  contracts: [],
+  account: "0x0",
+
   init: function() {
     console.log("App initialized...")
     return App.initWeb3();
@@ -12,8 +16,41 @@ App = {
       App.web3Provider = new Web3.providers.HttpProvider("http://localhost:7545");
       web3 = new Web3(App.web3Provider);
     }
-  }
 
+    return App.initContracts();
+  },
+
+  initContracts: function() {
+    $.getJSON("DappTokenSale.json", function(dappTokenSale) {
+      App.contracts.DappTokenSale = TruffleContract(dappTokenSale);
+
+      App.contracts.DappTokenSale.setProvider(App.web3Provider);
+      
+      App.contracts.DappTokenSale.deployed().then(function(dappTokenSale) {
+        console.log("Dapp Token Sale Address: ", dappTokenSale.address);
+      })
+    }).done(function() {
+      $.getJSON("DappToken.json", function(dappToken) {
+        App.contracts.DappToken = TruffleContract(dappToken);
+
+        App.contracts.DappToken.setProvider(App.web3Provider);
+        
+        App.contracts.DappToken.deployed().then(function(dappToken) {
+          console.log("Dapp Token Address: ", dappToken.address);
+        });
+        return App.render();
+      });
+    });
+  },
+
+  render: function() {
+    web3.eth.getCoinbase(function(err, account) {
+      if(err === null) {
+        App.account = account;
+        $("#accountAddress").html("Your Account: " + account);
+      }
+    })
+  } 
 }
 
 $(function() {

b6c4db90c27f50cf7172c09997c860755af194c9
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-02-24 22:04:28 -0400

    Create the basic HTML structure and initialize web3 on the app.js file

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,23 @@
-console.log("app.js loaded")
+App = {
+  init: function() {
+    console.log("App initialized...")
+    return App.initWeb3();
+  },
+
+  initWeb3: function() {
+    if(typeof web3 !== "undefined") {
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      App.web3Provider = new Web3.providers.HttpProvider("http://localhost:7545");
+      web3 = new Web3(App.web3Provider);
+    }
+  }
+
+}
+
+$(function() {
+  $(window).load(function() {
+    App.init();
+  })
+});
